ef91fb3aeb4bdb3f4883e1c0db174f8ecb001504
crazywhalecc <jesse2061@outlook.com>
2022-12-26 19:29:47 +0800

    add base client

---
+++ src/Choir/Http/Client/SwooleClient.php
@@ -0,0 +1,219 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Choir\Http\Client;
+
+use Choir\Http\Client\Exception\ClientException;
+use Choir\Http\Client\Exception\NetworkException;
+use Choir\Http\HttpFactory;
+use Choir\WebSocket\FrameFactory;
+use Choir\WebSocket\FrameInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\UriInterface;
+use Swoole\Coroutine;
+use Swoole\Coroutine\Http\Client;
+use Swoole\WebSocket\Frame;
+
+/**
+ * Swoole HTTP Client based on PSR-18.
+ */
+class SwooleClient implements TimeoutInterface, ClientInterface, AsyncClientInterface, UpgradableClientInterface
+{
+    /** @var null|Client Swoole Coroutine Client 对象 */
+    protected ?Client $client = null;
+
+    /** @var int TCP/WebSocket 连接状态标记 */
+    protected int $status = CHOIR_TCP_INITIAL;
+
+    /** @var null|callable|\Closure onMessage 回调 */
+    protected $on_message;
+
+    /** @var null|callable onClose 回调 */
+    protected $on_close;
+
+    private array $set = [];
+
+    /**
+     * @throws ClientException
+     */
+    public function __construct(array $set = [])
+    {
+        if (Coroutine::getCid() === -1) {
+            throw new ClientException('API must be called in the coroutine');
+        }
+        $this->withSwooleSet($set);
+    }
+
+    public function withSwooleSet(array $set = []): SwooleClient
+    {
+        if (!empty($set)) {
+            $this->set = $set;
+        }
+        return $this;
+    }
+
+    public function setTimeout(int $timeout)
+    {
+        $this->set['timeout'] = $timeout / 1000;
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        $this->client = $client = $this->buildBaseClient($request);
+        if ($client->errCode !== 0) {
+            throw new NetworkException($request, $client->errMsg, $client->errCode);
+        }
+        return HttpFactory::createResponse($client->statusCode, null, $client->getHeaders(), $client->getBody());
+    }
+
+    public function sendRequestAsync(RequestInterface $request, callable $success_callback, callable $error_callback): bool
+    {
+        go(function () use ($request, $success_callback, $error_callback) {
+            $this->client = $client = $this->buildBaseClient($request);
+            if ($client->errCode !== 0) {
+                call_user_func($error_callback, $request);
+            } else {
+                $response = HttpFactory::createResponse($client->statusCode, null, $client->getHeaders(), $client->getBody());
+                call_user_func($success_callback, $response);
+            }
+        });
+        return true;
+    }
+
+    /**
+     * 通过 PSR-7 的 Request 对象创建一个 Swoole Client
+     *
+     * @param RequestInterface $request PSR-7 Request 对象
+     */
+    public function buildBaseClient(RequestInterface $request): Client
+    {
+        $uri = $request->getUri();
+        $client = new Client($uri->getHost(), $uri->getPort() ?? ($uri->getScheme() === 'https' ? 443 : 80), $uri->getScheme() === 'https');
+        // 设置 Swoole 专有的 set 参数
+        $client->set($this->set);
+        // 设置 HTTP Method （POST、GET 等）
+        $client->setMethod($request->getMethod());
+        // 设置 HTTP Headers
+        $headers = [];
+        foreach ($request->getHeaders() as $name => $values) {
+            $headers[$name] = implode(', ', $values);
+        }
+        $client->setHeaders($headers);
+        // 如果是 POST 带 body，则设置 body
+        if (($data = $request->getBody()->getContents()) !== '') {
+            $client->setData($data);
+        }
+        $uri = $request->getUri()->getPath();
+        if ($uri === '') {
+            $uri = '/';
+        }
+        if (($query = $request->getUri()->getQuery()) !== '') {
+            $uri .= '?' . $query;
+        }
+        if (($fragment = $request->getUri()->getFragment()) !== '') {
+            $uri .= '?' . $fragment;
+        }
+        $client->execute($uri);
+        return $client;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function send($frame): bool
+    {
+        $swoole_frame = new Frame();
+        if ($frame instanceof FrameInterface) {
+            $swoole_frame->data = $frame->getData();
+            $swoole_frame->opcode = $frame->getOpcode();
+        } else {
+            $swoole_frame->data = $frame;
+        }
+        return (bool) $this->client->push($swoole_frame);
+    }
+
+    public function onMessage(callable $callback)
+    {
+        $this->on_message = $callback;
+    }
+
+    public function onClose(callable $callback)
+    {
+        $this->on_close = $callback;
+    }
+
+    public function upgrade(UriInterface $uri, array $headers = [], bool $reconnect = false): bool
+    {
+        if ($this->on_message === null) {
+            $this->on_message = function () {};
+        }
+        if ($this->on_close === null) {
+            $this->on_close = function () {};
+        }
+        if (!$reconnect && $this->status !== CHOIR_TCP_INITIAL) {
+            return false;
+        }
+        $this->status = CHOIR_TCP_CONNECTING;
+        $this->client = new Client($uri->getHost(), $uri->getPort() ?? ($uri->getScheme() === 'https' ? 443 : 80), $uri->getScheme() === 'https');
+        // 设置 Swoole 参数
+        $this->set['websocket_mask'] = true;
+        $this->client->set($this->set);
+        // 设置请求方法为 GET
+        $this->client->setMethod('GET');
+        // 设置 Headers
+        $headers_total = [];
+        foreach ($headers as $h_name => $header) {
+            if (is_array($header)) {
+                $headers_total[$h_name] = implode(', ', $header);
+            } else {
+                $headers_total[$h_name] = $header;
+            }
+        }
+        $this->client->setHeaders($headers_total);
+        // 设置请求的 URI
+        $uri_total = $uri->getPath();
+        if ($uri_total === '') {
+            $uri_total = '/';
+        }
+        if (($query = $uri->getQuery()) !== '') {
+            $uri_total .= '?' . $query;
+        }
+        if (($fragment = $uri->getFragment()) !== '') {
+            $uri_total .= '?' . $fragment;
+        }
+        $code = $this->client->upgrade($uri_total);
+        if ($this->client->errCode !== 0) {
+            return false;
+        }
+        if (!$code) {
+            $this->status = CHOIR_TCP_CLOSED;
+            return $code;
+        }
+        go(function () {
+            while (true) {
+                $result = $this->client->recv(60);
+                if ($result === false && !$this->client->connected) {
+                    $this->status = CHOIR_TCP_CLOSED;
+                    go(function () {
+                        $frame = FrameFactory::createCloseFrame($this->client->statusCode, '');
+                        call_user_func($this->on_close, $frame, $this);
+                    });
+                    break;
+                }
+                if ($result instanceof Frame) {
+                    go(function () use ($result) {
+                        $frame = new \Choir\WebSocket\Frame($result->data, $result->opcode, true, true);
+                        call_user_func($this->on_message, $frame, $this);
+                    });
+                }
+            }
+        });
+        $this->status = CHOIR_TCP_ESTABLISHED;
+        return $code;
+    }
+}
---
+++ src/Choir/Http/Client/TimeoutInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Choir\Http\Client;
+
+interface TimeoutInterface
+{
+    /**
+     * 设置 Client 的超时时间
+     *
+     * @param int $timeout 超时时间（毫秒）
+     */
+    public function setTimeout(int $timeout);
+}
